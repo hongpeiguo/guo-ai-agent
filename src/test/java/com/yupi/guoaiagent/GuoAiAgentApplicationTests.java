42041a3e31d3854cbdf857dcb9f7fe31dbcb438d
hongpeiguo <2287647807@qq.com>
2025-11-09 17:30:38 +0800

    项目创建

---
+++ src/test/java/com/yupi/guoaiagent/GuoAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupi.guoaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuoAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
